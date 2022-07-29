01bdf82ec0ded0738428a237cb6eb5e3d8705c9e
Samuel Thomas <samvthom16@gmail.com>
2022-07-29 10:06:47 +0530

    Refactor avatars animation

--- src/templates/Animation/MemberAvatars.vue
+++ src/templates/Animation/MemberAvatars.vue
@@ -1,11 +1,12 @@
 <template>
+  <div class="h-2 bg-lightgray rounded w-full mb-4"></div>
   <div class="animate-pulse flex space-x-3">
-    <div class="rounded-full bg-lightgray h-16 w-16"></div>
-    <div class="rounded-full bg-lightgray h-16 w-16"></div>
-    <div class="rounded-full bg-lightgray h-16 w-16"></div>
-    <div class="rounded-full bg-lightgray h-16 w-16"></div>
-    <div class="rounded-full bg-lightgray h-16 w-16"></div>
-    <div class="rounded-full bg-lightgray h-16 w-16"></div>
-    <div class="rounded-full bg-lightgray h-16 w-16"></div>
+    <div class="rounded-full bg-lightgray h-12 w-12"></div>
+    <div class="rounded-full bg-lightgray h-12 w-12"></div>
+    <div class="rounded-full bg-lightgray h-12 w-12"></div>
+    <div class="rounded-full bg-lightgray h-12 w-12"></div>
+    <div class="rounded-full bg-lightgray h-12 w-12"></div>
+    <div class="rounded-full bg-lightgray h-12 w-12"></div>
+    <div class="rounded-full bg-lightgray h-12 w-12"></div>
   </div>
 </template>
