f3e54eb1e563167adb2b295ee3087aad8c988231
Samuel Thomas <samvthom16@gmail.com>
2022-08-01 22:56:59 +0530

    #8 - use vue query for members & event UI page

---
+++ src/templates/Animation/PaginationLoader.vue
@@ -0,0 +1,9 @@
+<template>
+  <button class='bg-orange p-2 px-8 my-4 rounded text-white text-center mx-auto block'>
+    Loading
+    <Icon
+      type='CircularLoader'
+      class='inline h-4 w-4'
+    />
+  </button>
+</template>
