2909a206d49412d339eaf945a5a9a880a2193eb0
Samuel Thomas <samvthom16@gmail.com>
2023-05-11 23:43:06 +0530

    Stats UI refactor

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -2,7 +2,7 @@
   <div class="text-sm text-black font-semibold">AVERAGE COUNT</div>
 
   <div class="mb-4 text-base focus:outline-none sm:text-sm w-full  bg-lightergray rounded-lg drop-shadow-sm my-2 divide-y divide-lightgray">
-    <div class='p-4 px-6' v-for='stat in stats' :key='stat'>
+    <div class='p-4' v-for='stat in stats' :key='stat'>
       <div class='relative'>
         <div class='text-darkgray text-sm'>{{ stat.label }}</div>
         <div class='absolute right-0 top-0 text-md text-darkorange font-bold'>{{ stat.growth }}</div>
