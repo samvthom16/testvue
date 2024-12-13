685f651d58b27b73d98e43c8452867f70f0858d2
Suhail Bin Abdullah <suhail@mishantech.com>
2024-12-13 13:25:49 +0530

    group dropdown to checkboxes conversion

---
+++ src/components/CheckboxGroupField.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <label class="block font-semibold text-black mb-2">{{ field.label }}</label>
+    <div class="flex flex-wrap gap-4 items-center">
+      <div
+        v-for="(optionLabel, optionValue) in field.options"
+        :key="optionValue"
+        class="flex items-center"
+      >
+        <input
+          type="checkbox"
+          :id="`${field.id}-${optionValue}`"
+          :value="optionValue"
+          v-model="internalValue"
+          class="mr-2 border-2 border-solid border-black rounded"
+        />
+        <label :for="`${field.id}-${optionValue}`">{{ optionLabel }}</label>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CheckboxGroupField",
+  props: {
+    field: Object,
+    modelValue: Array,
+  },
+  emits: ["update:modelValue"],
+  data() {
+    return {
+      internalValue: this.modelValue || [],
+    };
+  },
+  watch: {
+    internalValue: {
+      handler(newValue) {
+        this.$emit("update:modelValue", newValue);
+      },
+      deep: true,
+    },
+    modelValue: {
+      handler(newValue) {
+        this.internalValue = newValue || [];
+      },
+      immediate: true,
+    },
+  },
+};
+</script>
--- src/lib/PostEdit.js
+++ src/lib/PostEdit.js
@@ -7,46 +7,46 @@ import Util from "@/lib/Util"
 import { useRoute } from 'vue-router';
 import { ref } from 'vue'
 
-const post_edit = ( post_type = 'inpursuit-members', textfields = {}, dropdownfields = {}, defaultPost = {} ) => {
+const post_edit = (post_type = 'inpursuit-members', textfields = {}, dropdownfields = {}, defaultPost = {}, checkboxFields = {}) => {
 
-  store.commit( 'getLocalSettings' )
+  store.commit('getLocalSettings')
 
-  const settings = ref( {} )
-  const fields = ref( [] )
+  const settings = ref({})
+  const fields = ref([])
   const route = useRoute()
-  const post = ref( defaultPost )
+  const post = ref(defaultPost)
 
-  for( var slug in textfields ){
+  for (var slug in textfields) {
 
     var type = 'text';
     var component = 'TextField';
 
-    switch( slug ){
+    switch (slug) {
       case 'date':
         type = 'date';
         break;
       case 'content':
         component = 'TextAreaField';
-        type      = '';
+        type = '';
     }
 
-    fields.value.push( {
-      type      : type,
-      id        : slug,
-      label     : textfields[ slug ],
-      component : component,
-      value     : ''
-    } )
+    fields.value.push({
+      type: type,
+      id: slug,
+      label: textfields[slug],
+      component: component,
+      value: ''
+    })
   }
 
   /*
   * PARSES SETTINGS TEXT FROM TAXONOMY SLUG & TERM ID
   */
-  const _getSettingsTextFromID = ( slug, id ) => {
+  const _getSettingsTextFromID = (slug, id) => {
 
     //console.log( 'settings => ' + slug + ' : ' + id )
 
-    if( settings.value[ slug ] && settings.value[ slug ][ id ] ) return settings.value[ slug ][ id ]
+    if (settings.value[slug] && settings.value[slug][id]) return settings.value[slug][id]
     return ''
   }
 
@@ -55,37 +55,38 @@ const post_edit = ( post_type = 'inpursuit-members', textfields = {}, dropdownfi
   * THIS COULD BE POST PASSED FROM PROPS
   * OR FETCHED FROM THE SERVER
   */
-  const feedFormFromPost = ( postData ) => {
+  const feedFormFromPost = (postData) => {
 
     post.value = postData
 
-
-    for( var i=0; i<fields.value.length; i++ ){
+    for (var i = 0; i < fields.value.length; i++) {
       var slug = fields.value[i]['id'];
 
-      //console.log( slug )
-
-      if( fields.value[i] && ( slug == 'title' || slug == 'content' ) ){
+      if (fields.value[i] && (slug == 'title' || slug == 'content')) {
 
         // CHECK FOR POST TITLE
         fields.value[i]['value'] = post.value[slug]['raw']
       }
-      else if ( fields.value[i] && slug == 'date' ) {
+      else if (fields.value[i] && slug == 'date') {
         // CHECK FOR DATE INFORMATION
-        var dateArr = post.value.date.split( 'T' );
-        if( dateArr.length ) fields.value[i]['value'] = dateArr[0]
+        var dateArr = post.value.date.split('T');
+        if (dateArr.length) fields.value[i]['value'] = dateArr[0]
       }
-      else if( post.value[ slug ] && Array.isArray( post.value[ slug ] ) ){
+      else if (post.value[slug] && Array.isArray(post.value[slug])) {
         // CHECK FOR CHECKBOX INFORMATION
-        fields.value[i]['value'] = _getSettingsTextFromID( slug, post.value[ slug ][0] )
+        if (slug === 'group')
+          fields.value[i]['value'] = post.value[slug];
+        else
+          fields.value[i]['value'] = _getSettingsTextFromID(slug, post.value[slug][0])
+
       }
-      else if ( post.value[ slug ] && fields.value[i].type && fields.value[i].type == 'text' ) {
+      else if (post.value[slug] && fields.value[i].type && fields.value[i].type == 'text') {
         // CHECK FOR TEXTBOXBOX INFORMATION
-        fields.value[i]['value'] = post.value[ slug ];
+        fields.value[i]['value'] = post.value[slug];
       }
-      else if( post.value[ slug ] ){
+      else if (post.value[slug]) {
         // CHECK FOR DROPDOWN INFORMATION
-        fields.value[i]['value'] = _getSettingsTextFromID( slug, post.value[ slug ] )
+        fields.value[i]['value'] = _getSettingsTextFromID(slug, post.value[slug])
       }
     }
 
@@ -96,34 +97,31 @@ const post_edit = ( post_type = 'inpursuit-members', textfields = {}, dropdownfi
   /*
   * REQUEST POST FROM SERVER BY POST ID
   */
-  const fetchPostFromServer = ( post_id ) => {
-    API.requestPost( post_type, post_id, { context: 'edit' } ).then(
-      ( response ) => feedFormFromPost( response.data )
+  const fetchPostFromServer = (post_id) => {
+    API.requestPost(post_type, post_id, { context: 'edit' }).then(
+      (response) => feedFormFromPost(response.data)
     )
   }
 
   /*
   * CREATE NEW POST
   */
-  const createPost = ( newPost ) => {
+  const createPost = (newPost) => {
 
     // ENABLE LOADING
-    store.commit( 'setProcessing', true )
+    store.commit('setProcessing', true)
 
-    //console.log( newPost )
 
-    API.createPost( newPost ).then(
-      ( response ) => {
+    API.createPost(newPost).then(
+      (response) => {
 
         // DISABLE LOADING
-        store.commit( 'setProcessing', false )
+        store.commit('setProcessing', false)
 
-        //console.log
-
-        redirectToSinglePost( response.data )
+        redirectToSinglePost(response.data)
       },
-      ( error ) => {
-        console.log( error.response )
+      (error) => {
+        console.log(error.response)
       }
     )
   }
@@ -131,87 +129,91 @@ const post_edit = ( post_type = 'inpursuit-members', textfields = {}, dropdownfi
   /*
   * UPDATE EXISTING POST
   */
-  const updatePost = ( newPost, post_id ) => {
+  const updatePost = (newPost, post_id) => {
 
     // ENABLE LOADING
-    store.commit( 'setProcessing', true )
+    store.commit('setProcessing', true)
 
     newPost['method'] = 'post'
-    API.requestPost( post_type, post_id, newPost ).then(
-      ( response ) => {
+    API.requestPost(post_type, post_id, newPost).then(
+      (response) => {
 
         // DISABLE LOADING
-        store.commit( 'setProcessing', false )
+        store.commit('setProcessing', false)
 
-        redirectToSinglePost( response.data )
+        redirectToSinglePost(response.data)
       },
-      ( error ) => {
-        console.log( error.response.data.message )
+      (error) => {
+        console.log(error.response.data.message)
       }
     )
   }
 
-  const createOrUpdatePost = ( post = {} ) => {
+  const createOrUpdatePost = (post = {}) => {
 
-    var newPost = Object.assign( {
-      post_type : post_type,
-      status    : 'publish'
-    }, post )
+    var newPost = Object.assign({
+      post_type: post_type,
+      status: 'publish'
+    }, post)
 
-    var form = document.querySelector( 'form' )
-    var formData = new FormData( form )
-    for ( var key of formData.keys() ) {
-      var value = formData.get( key )
-      if( value && value != "Choose" )
-        newPost[ key ] = value
+    var form = document.querySelector('form')
+    var formData = new FormData(form)
+    for (var key of formData.keys()) {
+      var value = formData.get(key)
+      if (value && value != "Choose")
+        newPost[key] = value
     }
+
+    fields.value.forEach((field) => {
+      if (field.component === "CheckboxGroupField" && Array.isArray(field.value)) {
+        newPost['inpursuit-group'] = field.value;
+      }
+    });
 
     // CHANGE THE FORMAT OF THE DATE
-    if( newPost.date ){
-      newPost.date = new Date( newPost.date ).toISOString();
+    if (newPost.date) {
+      newPost.date = new Date(newPost.date).toISOString();
     }
 
-    //console.log( newPost )
-
-    if( route.query && route.query.id ){
-      updatePost( newPost, route.query.id )
+    if (route.query && route.query.id) {
+      updatePost(newPost, route.query.id)
     }
-    else{
-      createPost( newPost )
+    else {
+      createPost(newPost)
     }
   }
 
   /*
   * DELETE POST FROM SERVER
   */
-  const deletePost = ( post_id ) => {
+  const deletePost = (post_id) => {
 
     // ENABLE LOADING
-    store.commit( 'setProcessing', true )
+    store.commit('setProcessing', true)
 
-    var params = { method  : 'delete' }
-    API.requestPost( post_type, post_id, params ).then( () => {
+    var params = { method: 'delete' }
+    API.requestPost(post_type, post_id, params).then(() => {
 
       // DISABLE LOADING
-      store.commit( 'setProcessing', false )
+      store.commit('setProcessing', false)
 
       redirectToPosts()
-    } )
+    })
   }
 
   /*
   * REDIRECT TO SINGLE POST AFTER UPDATING OR CREATING NEW POST
   */
-  const redirectToSinglePost = ( post ) => {
-    if( window.history.length > 2 ) router.go( -1 )
-    else router.push( Util.getPostLink( post ) )
+  const redirectToSinglePost = (post) => {
+    if (window.history.length > 2) router.go(-1)
+    else router.push(Util.getPostLink(post))
   }
 
   /*
   * REDIRECT TO MEMBERS LIST
   * MOST PORBABLY CALLED AFTER DELETING THE POST
   */
-  const redirectToPosts = () => router.push( { name : 'Members' } )
+  const redirectToPosts = () => router.push({ name: 'Members' })
 
   /*
   * SET FORM VALUES FROM THE POST RESPONSE
@@ -219,11 +221,11 @@ const post_edit = ( post_type = 'inpursuit-members', textfields = {}, dropdownfi
   * OR FETCH FROM THE SERVER
   */
   const setFormValues = () => {
-    if( route.query && route.query.params && route.query.params.post ){
-      feedFormFromPost( route.query.params.post )
+    if (route.query && route.query.params && route.query.params.post) {
+      feedFormFromPost(route.query.params.post)
     }
-    else if( route.query && route.query.id ){
-      fetchPostFromServer( route.query.id )
+    else if (route.query && route.query.id) {
+      fetchPostFromServer(route.query.id)
     }
   }
 
@@ -232,38 +234,52 @@ const post_edit = ( post_type = 'inpursuit-members', textfields = {}, dropdownfi
   */
   const fetchSettingsFromServer = () => {
 
-    Util.fetchSettings( ( data ) => {
+    Util.fetchSettings((data) => {
 
       settings.value = data
 
       // ITERATE THROUGH THE DROPDOWNFIELDS TO SET OPTIONS DATA
-      for( var key in dropdownfields ){
-        if( data[ key ] ){
-          let finalOptions = Object.assign( {
+      for (var key in dropdownfields) {
+        if (data[key]) {
+          let finalOptions = Object.assign({
             'Choose': 'Choose'
-          }, data[ key ] );
-          fields.value.push( {
-            id        : key,
-            label     : dropdownfields[ key ],
-            component : 'DropDownField',
-            value     : 'Choose',
-            options   : finalOptions
-          } )
+          }, data[key]);
+          fields.value.push({
+            id: key,
+            label: dropdownfields[key],
+            component: 'DropDownField',
+            value: 'Choose',
+            options: finalOptions
+          })
+        }
+      }
+
+      for (var key in checkboxFields) {
+        if (data[key]) {
+          let finalOptions = Object.assign({
+            // 'Choose': 'Choose'
+          }, data[key]);
+          fields.value.push({
+            id: key,
+            // label: dropdownfields[key] || key,
+            label: "Groups",
+            component: 'CheckboxGroupField',
+            value: [],
+            options: finalOptions
+          })
         }
       }
 
       // SET FORM VALUES FROM THE POST
       setFormValues()
 
-    } )
+    })
   }
 
   /*
   * FIRST FUNCTION TO BE CALLED
   */
   fetchSettingsFromServer()
-
-  //console.log( fields.value )
 
   return {
     post,
